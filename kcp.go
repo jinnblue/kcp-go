daddc59ba6c773b8f2bc72a3d766a3235701dfa8
xtaci <imap@live.com>
2025-12-05 19:06:08 +0800

    Revert "fix probe in recv"
    This reverts commit c22ac6f280ca2060c11ad8cd946e16e54204d045.

--- kcp.go
+++ kcp.go
@@ -433,8 +433,7 @@ func (kcp *KCP) Recv(buffer []byte) (n int) {
 	}
 
 	var fast_recover bool
-	minwnd := _imin_(kcp.rcv_wnd, kcp.rmt_wnd)
-	if kcp.rcv_queue.Len() >= int(minwnd) {
+	if kcp.rcv_queue.Len() >= int(kcp.rcv_wnd) {
 		fast_recover = true
 	}
 
@@ -469,7 +468,7 @@ func (kcp *KCP) Recv(buffer []byte) (n int) {
 	}
 
 	// fast recover
-	if kcp.rcv_queue.Len() < int(minwnd) && fast_recover {
+	if kcp.rcv_queue.Len() < int(kcp.rcv_wnd) && fast_recover {
 		// ready to send back IKCP_CMD_WINS in ikcp_flush
 		// tell remote my window size
 		kcp.probe |= IKCP_ASK_TELL
